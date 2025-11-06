21cc927105a8d59128b41a0f0fea7a6b1ea1bd65
Oier <ogarciam23@fpsanturtzilh.eus>
2025-11-06 09:29:44 +0100

    - Cookiarena aldatuta (orain gorde eta ezabatu ahal da).

--- backend/src/controller/getUserApiKey_rekin.php
+++ backend/src/controller/getUserApiKey_rekin.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-require_once __DIR__ . '/../DB.php';
+require_once __DIR__ . '/../../config/DB.php';
 require_once __DIR__ . '/../service/ErabiltzaileaService.php';
 
 $dbObj = new DB();
